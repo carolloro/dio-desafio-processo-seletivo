10c6b9d117706bedf2794b7d90736e75270c37fd
carolloro <carol.loro9@gmail.com>
2025-06-17 21:30:14 -0300

    Atualiza método imprimirSelecionados para exibir candidatos com índice e formato abreviado

--- desafio-processo-seletivo/src/candidatura/ProcessoSeletivo.java
+++ desafio-processo-seletivo/src/candidatura/ProcessoSeletivo.java
@@ -8,7 +8,23 @@ public class ProcessoSeletivo {
         //analisarCandidato(1900.0);
         //analisarCandidato(2200.0);
         //analisarCandidato(2000.0);
-        selecaoCandidatos();
+        //selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = {"João", "Maria", "Pedro", "Ana", "Lucas"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato  de nº " + (indice + 1) + " é " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado é " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
